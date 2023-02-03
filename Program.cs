a554c39df9a1fc7fbe9bedd8a459dcc526d04833
Joachim <ja@indicosys.com>
2023-02-03 17:57:26 +0100

    added cors

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+var AllowAllOrigin = "AllowOrigin";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowAllOrigin, pol => pol.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,6 +21,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseCors(AllowAllOrigin);
 
 app.UseHttpsRedirection();
 

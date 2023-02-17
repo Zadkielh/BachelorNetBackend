4390b0c2e383dc985b20837e88f3b5d142a6ef7f
Zadkielh <habbo_og_obbahfan@hotmail.com>
2023-02-17 12:30:04 +0100

    Update User

--- Model/User.cs
+++ Model/User.cs
@@ -3,6 +3,8 @@
     public class User
     {
         public int UserId { get; set; }
+
+        public string? UserName { get; set; }
         public DateOnly CreatedAt { get; set; }
 
         public int UserRoleId { get; set; }
